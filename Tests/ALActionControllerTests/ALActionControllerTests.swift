f144c1a41a54d8de2b191c13d4378b86f24217da
Anton Lobanov <alobanov11@gmail.com>
2021-01-28 11:37:21 +0300

    Add SPM

--- Tests/ALActionControllerTests/ALActionControllerTests.swift
+++ Tests/ALActionControllerTests/ALActionControllerTests.swift
@@ -6,7 +6,6 @@
 //
 
 import XCTest
-@testable import ALActionController
 
 class ALActionControllerTests: XCTestCase {
 
@@ -25,7 +24,7 @@ class ALActionControllerTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
