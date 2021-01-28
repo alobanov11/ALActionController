eb403f7ec84b15289e0791a1eaf24ae77e776214
Anton Lobanov <alobanov11@gmail.com>
2021-01-28 10:57:58 +0300

    Initial project

---
+++ ALActionController/ALActionController.h
@@ -0,0 +1,18 @@
+//
+//  ALActionController.h
+//  ALActionController
+//
+//  Created by Антон Лобанов on 28.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ALActionController.
+FOUNDATION_EXPORT double ALActionControllerVersionNumber;
+
+//! Project version string for ALActionController.
+FOUNDATION_EXPORT const unsigned char ALActionControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALActionController/PublicHeader.h>
+
+
